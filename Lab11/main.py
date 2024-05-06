c6a6f1bf4a690be3beeef9ed7952c116ae3b71cb
duseth <ilyas.salimov.07@gmail.com>
2024-05-06 18:08:39 +0300

    add lab11

--- Lab11/main.py
+++ Lab11/main.py
@@ -1,19 +1,18 @@
 from datetime import datetime, timedelta
+import requests
+import shutil
 
 import psycopg2
 import telebot
 from telebot import types
 
-# Подключение к базе данных PostgreSQL
 conn = psycopg2.connect(database="postgres", user="postgres", password="12345", host="localhost",
                         port="5432")
 cursor = conn.cursor()
 
-# Создание бота
-bot = telebot.TeleBot("API")
+bot = telebot.TeleBot("6578758432:AAEMsfm8pmxByxFKUp11oBGz4ecfWXSQGVk")
 
 
-# Команда /start или /help
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message,
@@ -21,7 +20,6 @@ def send_welcome(message):
     send_week_buttons(message.chat.id)
 
 
-# Команда /week
 @bot.message_handler(commands=['week'])
 def current_week(message):
     today = datetime.now()
@@ -30,7 +28,17 @@ def current_week(message):
     bot.reply_to(message, f"Сейчас {parity} неделя.")
 
 
-# Команда хочу
+@bot.message_handler(commands=['cat'])
+def current_week(message):
+    r = requests.get("https://cataas.com/cat", stream=True)
+    if r.status_code == 200:
+        with open("cat.jpg", 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+    bot.send_photo(message.chat.id, photo=open('cat.jpg', 'rb'))
+
+
 @bot.message_handler(content_types=["text"])
 def want_handler(message):
     if message.text.lower() == "хочу":
@@ -39,19 +47,16 @@ def want_handler(message):
         bot.send_message(message.chat.id, "Извините, я Вас не понял")
 
 
-# Команда /kstu
 @bot.message_handler(commands=['kstu'])
 def kstu_link(message):
     bot.reply_to(message, "Ссылка на КНИТУ: https://www.kstu.ru")
 
 
-# Обработка неизвестных команд
 @bot.message_handler(func=lambda message: True)
 def unknown_command(message):
     bot.reply_to(message, "Извините, я Вас не понял.")
 
 
-# Функция для получения расписания на определенный день
 def get_schedule(day):
     cursor.execute(
         "SELECT timetable.subject, room_number, start_time, full_name FROM timetable INNER JOIN subject ON timetable.subject = subject.name INNER JOIN teacher ON subject.name = teacher.subject WHERE day = %s",
@@ -60,7 +65,6 @@ def get_schedule(day):
     return schedule
 
 
-# Функция для форматирования расписания
 def format_schedule(schedule):
     formatted_schedule = ""
     for entry in schedule:
@@ -69,7 +73,6 @@ def format_schedule(schedule):
     return formatted_schedule.strip()
 
 
-# Обработчик нажатий на кнопки дней недели
 @bot.callback_query_handler(func=lambda call: call.data in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'])
 def callback_handler(call):
     day_mapping = {'monday': 'Понедельник', 'tuesday': 'Вторник', 'wednesday': 'Среда', 'thursday': 'Четверг',
@@ -83,7 +86,6 @@ def callback_handler(call):
         bot.send_message(call.message.chat.id, "Расписание на этот день отсутствует.")
 
 
-# Обработчик нажатия на кнопку текущей недели
 @bot.callback_query_handler(func=lambda call: call.data == 'current_week')
 def current_week_schedule(call):
     send_week_buttons(call.message.chat.id)
@@ -93,7 +95,6 @@ def current_week_schedule(call):
     bot.send_message(call.message.chat.id, f"Расписание на эту неделю:\n{formatted_schedule}")
 
 
-# Обработчик нажатия на кнопку следующей недели
 @bot.callback_query_handler(func=lambda call: call.data == 'next_week')
 def next_week_schedule(call):
     next_week = datetime.now() + timedelta(days=7)
@@ -103,7 +104,6 @@ def next_week_schedule(call):
     bot.send_message(call.message.chat.id, f"Расписание на следующую неделю:\n{formatted_schedule}")
 
 
-# Функция для форматирования расписания на всю неделю
 def format_week_schedule(week_schedule):
     day_mapping = {'monday': 'Понедельник', 'tuesday': 'Вторник', 'wednesday': 'Среда', 'thursday': 'Четверг',
                    'friday': 'Пятница', 'saturday': 'Суббота', 'sunday': 'Воскресенье'}
@@ -114,7 +114,6 @@ def format_week_schedule(week_schedule):
     return formatted_schedule.strip()
 
 
-# Функция для получения расписания на всю неделю
 def get_week_schedule(start_date):
     day_mapping = {'monday': 'Понедельник', 'tuesday': 'Вторник', 'wednesday': 'Среда', 'thursday': 'Четверг',
                    'friday': 'Пятница', 'saturday': 'Суббота', 'sunday': 'Воскресенье'}
@@ -127,7 +126,6 @@ def get_week_schedule(start_date):
     return week_schedule
 
 
-# Функция для отправки кнопок выбора дня недели
 def send_week_buttons(chat_id):
     markup = types.InlineKeyboardMarkup()
     day_mapping = {'monday': 'Понедельник', 'tuesday': 'Вторник', 'wednesday': 'Среда', 'thursday': 'Четверг',
@@ -139,5 +137,4 @@ def send_week_buttons(chat_id):
     bot.send_message(chat_id, "Выберите день недели или неделю:", reply_markup=markup)
 
 
-# Запуск бота
 bot.polling()
